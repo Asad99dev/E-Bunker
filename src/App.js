29162945a882a5e92241fb6bb5c9e20ff567c73b
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2021-12-29 23:10:04 +0000

    Updated Navbar Styling

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App() {
           <Route path="/phishing-learn" element={<PhishingLearn />} />
           <Route path="/phishing-quiz" element={<PhishingQuiz />} />
           <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Router>
     </>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,55 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
-
+import "./Navbar.css";
 
 function Navigation() {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar bg="dark" expand="lg">
+    <Navbar
+      bg="dark"
+      expand="lg"
+      variant="dark"
+      sticky="top"
+      expanded={expanded}
+    >
       <Container>
         <Navbar.Brand>
-          <Link to="/" className="nav-link">
-          <i class="fas fa-user-shield" /> E-Bunker
+          <Link to="/" className="home-link" onClick={() => setExpanded(false)}>
+            <i class="fas fa-user-shield" /> E-Bunker
           </Link>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="nasic-navbar-nav">
-          <Nav className="nav-link">
-            <Link to="/" className="nav-link">
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(!expanded)}
+        />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Link
+              to="/"
+              className="nav-link"
+              onClick={() => setExpanded(false)}
+            >
               Home
             </Link>
             <NavDropdown title="Phishing" id="basic-nav-dropdown">
               <NavDropdown.Item>
-                <Link to="/phishing-learn" classname="dropdown-link">
-                  Learn
+                <Link to="/phishing-learn" className="nav-link" onClick={() => setExpanded(false)}>
+                  Learn 
                 </Link>
               </NavDropdown.Item>
               <NavDropdown.Item>
-                <Link to="/phishing-quiz" classname="dropdown-link">
+                <Link to="/phishing-quiz" className="nav-link" onClick={() => setExpanded(false)}>
                   Quiz
                 </Link>
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Malware" id="basic-nav-dropdown">
               <NavDropdown.Item>
-                <Link to="/malware-learn" classname="dropdown-link">
+                <Link to="/malware-learn" className="nav-link" onClick={() => setExpanded(false)}>
                   Learn
                 </Link>
               </NavDropdown.Item>
               <NavDropdown.Item>
-                <Link to="/malware-quiz" classname="dropdown-link">
+                <Link to="/malware-quiz" className="nav-link" onClick={() => setExpanded(false)}>
                   Quiz
                 </Link>
               </NavDropdown.Item>
             </NavDropdown>
-            <Link to="/about" className="nav-link">
+            <Link
+              to="/about"
+              className="nav-link"
+              onClick={() => setExpanded(false)}
+            >
               About
             </Link>
-            <Link to="/sign-in" className="nav-link">
+            <Link
+              to="/sign-in"
+              className="nav-link"
+              onClick={() => setExpanded(false)}
+            >
               Sign In
-            </Link>
-            <Link to="/sign-up" className="nav-link">
-              Sign Up
             </Link>
           </Nav>
         </Navbar.Collapse>
