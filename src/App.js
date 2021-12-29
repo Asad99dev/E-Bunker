96e02c801960558f688b9f60ea2b64c6c6805070
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2021-12-29 17:45:28 +0000

    Added routes to pages

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ import Navigation from "./Components/Navbar";
 import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import About from "./Pages/About.js";
+import MalwareLearn from "./Pages/MalwareLearn.js";
+import MalwareQuiz from "./Pages/MalwareQuiz.js";
+import PhishingLearn from "./Pages/PhishingLearn.js";
+import PhishingQuiz from "./Pages/PhishingQuiz.js";
+import SignIn from "./Pages/SignIn.js";
+import SignUp from "./Pages/SignUp.js";
+
 function App() {
   return (
-    <Router>
-      <Navigation />
-    </Router>
+    <>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/malware-learn" element={<MalwareLearn />} />
+          <Route path="/malware-quiz" element={<MalwareQuiz />} />
+          <Route path="/phishing-learn" element={<PhishingLearn />} />
+          <Route path="/phishing-quiz" element={<PhishingQuiz />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Pages/PhishingLearn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PhishingLearn() {
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default PhishingLearn;
