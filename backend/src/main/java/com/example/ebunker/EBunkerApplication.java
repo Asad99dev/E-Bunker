4ff355007058d715d5dd73612d4f784ec5c03610
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2021-12-31 18:17:26 +0000

    Changed names

--- backend/src/main/java/com/example/ebunker/EBunkerApplication.java
+++ backend/src/main/java/com/example/ebunker/EBunkerApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.ebunker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EBunkerBackendApplication {
+public class EBunkerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EBunkerBackendApplication.class, args);
+		SpringApplication.run(EBunkerApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/example/ebunker/EBunkerApplicationTests.java
+++ backend/src/test/java/com/example/ebunker/EBunkerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.ebunker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EBunkerBackendApplicationTests {
+class EBunkerApplicationTests {
 
 	@Test
 	void contextLoads() {
