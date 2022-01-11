8ab16d17ab1c673ff5586ed2e7b292755be43e09
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2022-01-11 22:18:05 +0000

    Updated Hero component

--- frontend/src/Components/Hero.js
+++ frontend/src/Components/Hero.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+import logo from "../Images/logoHero.svg";
+import "./Hero.css";
 
 function Hero() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  return (
+    <div className="hero-container">
+      <img className="logo" src={logo} alt="e-bunker"/>
+      <h1>Helping you browse with confidence</h1>
+      <p>Get started by signing in or creating an account with us</p>
+      <div className="hero-btns">
+        <Button
+          className="btn-hero"
+          handleClick={() => {
+            navigate("/sign-in");
+          }}
+          label="Sign In"
+        />
 
-    return (
-        <div>
-        <h1>E-Bunker</h1>
-        <button onClick={() => {navigate("/sign-in");}}>
-            Sign In
-        </button>
-
-            
-        </div>
-    )
+        <Button
+          className="btn-hero"
+          handleClick={() => {
+            navigate("/sign-up");
+          }}
+          label="Sign Up"
+        />
+      </div>
+      <p>Alternatively explore our site to see what we can do for you</p>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
