abdb4cade72042c18c610920fa43b4ca29a17a39
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2022-01-14 22:20:45 +0000

    Added content

--- frontend/src/Pages/Phishing.js
+++ frontend/src/Pages/Phishing.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../Components/Button.js";
 
 function Phishing() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h2>Hello</h2>
+    <div className="bg-container">
+      <h1>Phishing</h1>
+      <p>
+        As technology continues to evolve, phishing shows no sign of slowing
+        down.
+      </p>
+      <p>
+        Click the link below to start learning, or if you feel up to the
+        challenge, how about taking on our quiz!
+      </p>
+      <div className="bg-btns">
+        <Button
+          className="btn-bg"
+          handleClick={() => {
+            navigate("/phishing-learn");
+          }}
+          label="Learn"
+        />
+
+        <Button
+          className="btn-bg"
+          handleClick={() => {
+            navigate("/phishing-quiz");
+          }}
+          label="Quiz"
+        />
+      </div>
     </div>
   );
 }
