3da38566f0788a4fc984182bc1819caefa2139aa
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2022-02-04 22:54:45 +0000

    Added content

--- frontend/src/Pages/PhishingLearn.js
+++ frontend/src/Pages/PhishingLearn.js
@@ -8,6 +8,15 @@ function PhishingLearn() {
   return (
     <div className="pg-container">
       <h1>What is phishing?</h1>
+      <br />
+      <p>
+        Phishing is described as a type of social engineering whereby a
+        potential cyber criminal sends a fraudulent message to deceive them into
+        revealing sensitive information or to infect their device with malware.
+        Phishing is also one of the most common cybersecurity threats as they
+        are easy to prepare and a large majority of the population can be
+        susceptible attacks through even one moment of uncertainty.
+      </p>
     </div>
   );
 }
