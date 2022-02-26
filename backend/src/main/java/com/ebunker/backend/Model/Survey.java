4cece58ceca55fa50a84b55af881af72b70f34fa
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2022-02-26 13:40:56 +0000

    Updated survey model

--- backend/src/main/java/com/ebunker/backend/Model/Survey.java
+++ backend/src/main/java/com/ebunker/backend/Model/Survey.java
@@ -1,149 +1,146 @@
 package com.ebunker.backend.Model;
 
-import java.util.HashMap;
-import java.util.Map;
-import javax.annotation.Generated;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import javax.persistence.*;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-"q1",
-"q2",
-"q4",
-"q5",
-"q6",
-"q7",
-"q8",
-"q9",
-"q10"
-})
-@Generated("jsonschema2pojo")
+@Entity
+@Table(name = "survey")
+
 public class Survey {
 
-@JsonProperty("q1")
-private String q1;
-@JsonProperty("q2")
-private String q2;
-@JsonProperty("q4")
-private String q4;
-@JsonProperty("q5")
-private String q5;
-@JsonProperty("q6")
-private String q6;
-@JsonProperty("q7")
-private String q7;
-@JsonProperty("q8")
-private String q8;
-@JsonProperty("q9")
-private String q9;
-@JsonProperty("q10")
-private String q10;
-@JsonIgnore
-private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-@JsonProperty("q1")
-public String getQ1() {
-return q1;
-}
+    @Column
+    private int q1;
 
-@JsonProperty("q1")
-public void setQ1(String q1) {
-this.q1 = q1;
-}
+    @Column
+    private int q2;
 
-@JsonProperty("q2")
-public String getQ2() {
-return q2;
-}
+    @Column
+    private int q3;
 
-@JsonProperty("q2")
-public void setQ2(String q2) {
-this.q2 = q2;
-}
+    @Column
+    private int q4;
 
-@JsonProperty("q4")
-public String getQ4() {
-return q4;
-}
+    @Column
+    private int q5;
 
-@JsonProperty("q4")
-public void setQ4(String q4) {
-this.q4 = q4;
-}
+    @Column
+    private int q6;
 
-@JsonProperty("q5")
-public String getQ5() {
-return q5;
-}
+    @Column
+    private int q7;
 
-@JsonProperty("q5")
-public void setQ5(String q5) {
-this.q5 = q5;
-}
+    @Column
+    private int q8;
 
-@JsonProperty("q6")
-public String getQ6() {
-return q6;
-}
+    @Column
+    private int q9;
 
-@JsonProperty("q6")
-public void setQ6(String q6) {
-this.q6 = q6;
-}
+    @Column
+    private int q10;
 
-@JsonProperty("q7")
-public String getQ7() {
-return q7;
-}
+    public Survey(int q1, int q2, int q3, int q4, int q5, int q6, int q7, int q8, int q9, int q10) {
+        this.q1 = q1;
+        this.q2 = q2;
+        this.q3 = q3;
+        this.q4 = q4;
+        this.q5 = q5;
+        this.q6 = q6;
+        this.q7 = q7;
+        this.q8 = q8;
+        this.q9 = q9;
+        this.q10 = q10;
 
-@JsonProperty("q7")
-public void setQ7(String q7) {
-this.q7 = q7;
-}
+    }
 
-@JsonProperty("q8")
-public String getQ8() {
-return q8;
-}
+    public Long getId() {
+        return id;
+    }
 
-@JsonProperty("q8")
-public void setQ8(String q8) {
-this.q8 = q8;
-}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-@JsonProperty("q9")
-public String getQ9() {
-return q9;
-}
+    public int getQ1() {
+        return q1;
+    }
 
-@JsonProperty("q9")
-public void setQ9(String q9) {
-this.q9 = q9;
-}
+    public void setQ1(int q1) {
+        this.q1 = q1;
+    }
 
-@JsonProperty("q10")
-public String getQ10() {
-return q10;
-}
+    public int getQ2() {
+        return q2;
+    }
 
-@JsonProperty("q10")
-public void setQ10(String q10) {
-this.q10 = q10;
-}
+    public void setQ2(int q2) {
+        this.q2 = q2;
+    }
 
-@JsonAnyGetter
-public Map<String, Object> getAdditionalProperties() {
-return this.additionalProperties;
-}
+    public int getQ3() {
+        return q3;
+    }
 
-@JsonAnySetter
-public void setAdditionalProperty(String name, Object value) {
-this.additionalProperties.put(name, value);
-}
+    public void setQ3(int q3) {
+        this.q3 = q3;
+    }
+
+    public int getQ4() {
+        return q4;
+    }
+
+    public void setQ4(int q4) {
+        this.q4 = q4;
+    }
+
+    public int getQ5() {
+        return q5;
+    }
+
+    public void setQ5(int q5) {
+        this.q5 = q5;
+    }
+
+    public int getQ6() {
+        return q6;
+    }
+
+    public void setQ6(int q6) {
+        this.q6 = q6;
+    }
+
+    public int getQ7() {
+        return q7;
+    }
+
+    public void setQ7(int q7) {
+        this.q7 = q7;
+    }
+
+    public int getQ8() {
+        return q8;
+    }
+
+    public void setQ8(int q8) {
+        this.q8 = q8;
+    }
+
+    public int getQ9() {
+        return q9;
+    }
+
+    public void setQ9(int q9) {
+        this.q9 = q9;
+    }
+
+    public int getQ10() {
+        return q10;
+    }
+
+    public void setQ10(int q10) {
+        this.q10 = q10;
+    }
 
 }
