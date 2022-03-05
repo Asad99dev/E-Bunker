77fb05692201654a46aa920a27250dc63dd45b75
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2022-03-05 21:58:31 +0000

    Added response to API

--- backend/src/main/java/com/ebunker/backend/Controller/SurveyController.java
+++ backend/src/main/java/com/ebunker/backend/Controller/SurveyController.java
@@ -21,13 +21,21 @@ public class SurveyController {
     SurveyRepository surveyRepository;
 
     @PostMapping("/submit")
-    public String submitSurvey (@Valid @RequestBody Survey survey) {
+    public String submitSurvey(@Valid @RequestBody Survey survey) {
 
-        surveyRepository.save(survey);
+        Survey submitted = surveyRepository.save(survey);
 
-        return "Submitted";
+        if (submitted.getTotal() <= 15) {
+            return "Your cybersecurity score is quite low, we recommend you take immediate action to protect your accounts and devices. Think of the things in the survey you never or rarely do then try and make changes so that you do them more often";
+        } else if (submitted.getTotal() <= 35) {
+
+            return "You have an average cybersecurity score, you do take care to protect your devices and accounts to an extent but there is room for improvement. For extra peace of mind, we recommend you go through all the protections you have in place and see if you can make any changes to stay safer";
+
+        } else {
+
+            return "You have an excellent cybersecurity score, you take the utmost care in protecting your accounts and devices and there is not much you can do to improve. We recommend you keep this up and make sure to schedule checks regularly to make sure your protections are up to date";
+        }
 
     }
-
 
 }
