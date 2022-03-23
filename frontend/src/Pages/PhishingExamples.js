92c0e7664dfadddbf1e6ca9fcd8353dab0b8e7e0
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2022-03-23 20:14:36 +0000

    Added displaying of images

--- frontend/src/Pages/PhishingExamples.js
+++ frontend/src/Pages/PhishingExamples.js
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from "react";
 import AuthService from "../Services/auth.service";
 import FileUpload from "../Components/FileUpload";
+import axios from "axios";
 
 function PhishingExamples() {
   const [currentUser, setCurrentUser] = useState(undefined);
+  const [images, setImages] = useState([]);
+
+  const url = `http://localhost:8080/files`;
 
   useEffect(() => {
     const user = AuthService.getCurrentUser();
     if (user) {
       setCurrentUser(user);
     }
+
+    getImages();
   }, []);
+
+  const getImages = () => {
+    axios
+      .get(url, {
+        headers: {
+          "Content-type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        const currentImage = response.data[1].url;
+        setImages(currentImage);
+      });
+  };
 
   return (
     <div className="bg-container">
@@ -21,6 +41,9 @@ function PhishingExamples() {
       ) : (
         <p>Sign in to upload your own pictures</p>
       )}
+      <div>
+        <img src={images} />
+      </div>
     </div>
   );
 }
