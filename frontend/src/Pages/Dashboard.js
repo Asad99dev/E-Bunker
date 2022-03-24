63ead247c10c58edcf8b86db92328cf113944409
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2022-03-24 14:19:12 +0000

    Added list of images

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import AuthService from "../Services/auth.service";
+import axios from "axios";
 
 function Dashboard() {
   const navigate = useNavigate();
+  const currentUser = AuthService.getCurrentUser();
+  const [images, getImages] = useState([]);
 
   const user = JSON.parse(localStorage.getItem("user"));
   const admin = ["ROLE_ADMIN", "ROLE_USER"];
@@ -13,9 +17,36 @@ function Dashboard() {
     if (userString !== adminString) {
       navigate("/unauthorized");
     }
-  });
 
-  return <div className="bg-container"></div>;
+    getAllImages();
+  }, []);
+
+  const getAllImages = () => {
+    axios
+      .get(`http://localhost:8080/files`, {
+        headers: {
+          "Content-type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        const currentImages = response.data;
+        getImages(currentImages);
+      })
+      .catch((error) => {
+        console.log(error.data);
+      });
+  };
+
+  return (
+    <div className="bg-container">
+      {images.map((image) => (
+        <div className="content-container" key={image.url}>
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Dashboard;
