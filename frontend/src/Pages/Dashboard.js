bb50d6f34211551097dac3cd74567ce88295e4b0
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2022-03-24 17:49:26 +0000

    test

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -7,11 +7,13 @@ function Dashboard() {
   const navigate = useNavigate();
   const currentUser = AuthService.getCurrentUser();
   const [images, getImages] = useState([]);
+  const [refresh, setRefresh] = useState(false);
 
   const user = JSON.parse(localStorage.getItem("user"));
   const admin = ["ROLE_ADMIN", "ROLE_USER"];
   const userString = user.roles.toString();
   const adminString = admin.toString();
+  
 
   useEffect(() => {
     if (userString !== adminString) {
@@ -38,11 +40,31 @@ function Dashboard() {
       });
   };
 
+  function deleteImage(event) {
+    event.preventDefault();
+
+    return axios.delete(`http://localhost:8080/delete/files/${images.url}`, {
+      headers: {
+        Authorization: "Bearer " + currentUser.accessToken,
+      }
+    })
+    .then((response) => {
+      console.log(response.data);
+      setRefresh(true);
+    })
+    .catch((error) => {
+      console.log(error.data);
+    });
+  }
+
   return (
     <div className="bg-container">
       {images.map((image) => (
         <div className="content-container" key={image.url}>
           <p>{image.name}</p>
+          <button className="btn-bg" onClick={deleteImage}>
+            Delete
+          </button>
         </div>
       ))}
     </div>
